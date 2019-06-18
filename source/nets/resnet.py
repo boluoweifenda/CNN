51c4f1b6917deeb28b0fea8ef616b5e2d27f58e7
boluoweifenda <wushuang0717@gmail.com>
2019-06-18 17:20:08 +0800

    minor

--- source/nets/resnet.py
+++ source/nets/resnet.py
@@ -4,41 +4,42 @@ from nets.net import Net
 
 class ResNet(Net):
 
-  def _residual(self, x, c_out, stride=1, bottleneck=False, first=False):
+  # pre-activation residual block
+  def _residual(self, x, c_out, stride=1, bottleneck=False):
     c_in = self.get_shape(x)[1]
     orig_x = x
 
-    # pre-activation residual block
-    if first is False:
+    if bottleneck:
       with tf.variable_scope('S0'):
         x = self.batch_norm(x)
         x = self.activation(x)
-    if bottleneck:
-      with tf.variable_scope('S0'):
         x = self.conv(x, 1, c_out / 4)
       with tf.variable_scope('S1'):
         x = self.batch_norm(x)
         x = self.activation(x)
-        # we use stride 2 in the 3x3 conv when using bottleneck following fb.resnet.torch
-        x = self.conv(x, 3, c_out / 4, stride)
+        x = self.conv(x, 3, c_out / 4, stride)  # stride 2 in 3x3 conv following fb.resnet.torch
       with tf.variable_scope('S2'):
         x = self.batch_norm(x)
         x = self.activation(x)
         x = self.conv(x, 1, c_out)
     else:
       with tf.variable_scope('S0'):
+        x = self.batch_norm(x)
+        x = self.activation(x)
         x = self.conv(x, 3, c_out, stride)
       with tf.variable_scope('S1'):
         x = self.batch_norm(x)
         x = self.activation(x)
         x = self.conv(x, 3, c_out)
 
-    # x = self.squeeze_and_excitation(x, r=4)
+    # x = self.squeeze_and_excitation(x, r=16)
 
     with tf.variable_scope('SA'):
-      if stride is not 1 or c_in != c_out:
-        # # Option B
-        orig_x = self.conv(orig_x, 1, c_out, stride)
+      if stride is not 1:
+        # suggested by "Bag of Tricks for Image Classification with Convolutional Neural Networks"
+        orig_x = self.pool(x, 'AVG', 2, 2, padding='VALID')
+      if c_in != c_out:
+        orig_x = self.conv(orig_x, 1, c_out)
       x += orig_x
 
     return x
@@ -50,7 +51,7 @@ class ResNet(Net):
     if self.dataset in ['cifar10', 'cifar100']:
 
       num_residual = 9  # totoal layer: 6n+2 / 9n+2
-      bottleneck = False
+      bottleneck = True
       strides = [1, 2, 2]
       filters = [16, 32, 64]
 
@@ -88,17 +89,13 @@ class ResNet(Net):
       with tf.variable_scope('init'):
         if self.dataset == 'imagenet':
           x = self.conv(x, 7, 64, stride=2)
-          x = self.batch_norm(x)
-          x = self.activation(x)
           x = self.pool(x, type='MAX', ksize=3, stride=2)
         else:
           x = self.conv(x, 3, 64)
-          x = self.batch_norm(x)
-          x = self.activation(x)
 
       for i in range(len(strides)):
         with tf.variable_scope('U%d-0' % i):
-          x = self._residual(x, filters[i], strides[i], bottleneck, first=True if i == 0 else False)
+          x = self._residual(x, filters[i], strides[i], bottleneck)
         for j in range(1, num_residual[i]):
           with tf.variable_scope('U%d-%d' % (i, j)):
             x = self._residual(x, filters[i], 1, bottleneck)
