042d0e25d683a965a5f5873954c79df199ac67b9
wushuang <wus15@mails.tsinghua.edu.cn>
2019-06-22 18:57:25 +0800

    add mobilenet_v2 and shufflenet_v2 training logs

--- source/options/mobilenet_v2.py
+++ source/options/mobilenet_v2.py
@@ -1,22 +1,22 @@
 import tensorflow as tf
 
-title = 'ResNet-50'  # top1 error rate: 23.6%
+title = 'mobilenet-v2, 1.4x'  # top1 error rate: 25.8%
 mode = 'train'  # 'train', 'test', 'restart', 'debug', 'export', 'input_train', 'input_test', 'speed_net'
 seed = None
 repeat = 1
 delay = False  # start training after xxx minutes
-gpu_list = [0, 1, 2, 3]
+gpu_list = [0, 1]
 batch_size = 256
 data_format = 'NCHW'
 
-dataset = 'imagenet'  # 'mnist','svhn','cifar10', 'cifar100', 'imagenet', 'fashion'
-preprocess = 'inception_v2'  # 'mnist', 'cifar', 'alexnet', 'inception_v2'
-network = 'resnet'  # 'mlp', 'lenet', 'alexnet', 'resnet', 'densenet', 'mobilenet_v1', 'mobilenet_v2', shufflenet_v2
+dataset = 'imagenet'  # 'mnist','svhn','cifar10', 'cifar100', 'imagenet', 'fashion', 'tiny_imagenet'
+preprocess = 'inception'  # 'mnist', 'cifar', 'alexnet', 'inception'
+network = 'mobilenet_v2'  # 'mlp', 'lenet', 'alexnet', 'resnet', 'densenet', 'mobilenet_v1', 'mobilenet_v2', shufflenet_v2
 
 path_load = None
 path_save = True   # None, False, True, or specify a dir
 
-l2_decay = {'decay': 1e-4, 'exclude': []}
+l2_decay = {'decay': 0.4e-4, 'exclude': ['depthwise', 'batchnorm', 'bias']}
 epoch_step = tf.Variable(1, name='epoch_step', trainable=False)
 learning_step = tf.Variable(0, name='learning_step', trainable=False)
 lr_decay = tf.train.cosine_decay(0.1, epoch_step, decay_steps=100)
