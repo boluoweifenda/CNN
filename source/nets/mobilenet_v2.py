28ad68f5e605243c30eed022d7ec503493493ebc
boluoweifenda <wushuang0717@gmail.com>
2019-06-23 09:12:28 +0800

    minor

--- source/nets/mobilenet_v2.py
+++ source/nets/mobilenet_v2.py
@@ -29,31 +29,59 @@ class MobileNet(Net):
 
   def model(self, x):
 
-    Stride = [1, 2, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1]
-    # Out = [32, 16, 24, 24, 32, 32, 32, 64, 64, 64, 64,  96,  96,  96, 160, 160, 160, 320, 1280]  # 1.0x
-    Out = [48, 24, 32, 32, 48, 48, 48, 88, 88, 88, 88, 136, 136, 136, 224, 224, 224, 448, 1792]  # 1.4x
-    Expansion = [1, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
+    if self.dataset in ['cifar10', 'cifar100']:
+      num_residual = 12
+      Stride = [1, 2, 2]
+      Out = [16, 32, 64]
+      Expansion = [6, 6, 6]
 
-    with tf.variable_scope('init'):
-      x = self.conv(x, 3, Out[0], stride=2)
-      x = self.batch_norm(x)
-      x = self.activation(x)
+      with tf.variable_scope('init'):
+        x = self.conv(x, 3, Out[0])
+        x = self.batch_norm(x)
+        x = self.activation(x)
 
-    for i in range(len(Stride)):
-      x = self._expanded_conv(x, 3, Out[i+1], Stride[i], Expansion[i], name='block%d' % i)
+      for i in range(len(Stride)):
+        for j in range(num_residual):
+          x = self._expanded_conv(x, 3, Out[i], stride=Stride[i] if j == 0 else 1, expansion_size=Expansion[i], name='block%d-%d' % (i,j))
 
-    with tf.variable_scope('last'):
-      x = self.conv(x, 1, Out[-1])
-      x = self.batch_norm(x)
-      x = self.activation(x)
-    with tf.variable_scope('global_avg_pool'):
-      x = self.pool(x, 'GLO')
+      with tf.variable_scope('last'):
+        x = self.conv(x, 1, Out[-1]*6)
+        x = self.batch_norm(x)
+        x = self.activation(x)
+      with tf.variable_scope('global_avg_pool'):
+        x = self.pool(x, 'GLO')
 
-    with tf.variable_scope('logit'):
-      x = self.dropout(x, 0.2)
-      x = self.fc(x, self.shape_y[1], name='fc', bias=True)
+      with tf.variable_scope('logit'):
+        x = self.fc(x, self.shape_y[1], name='fc')
 
-    return x
+      return x
+
+    elif self.dataset in ['imagenet', 'tiny_imagenet']:
+      Stride = [1, 2, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1]
+      # Out = [32, 16, 24, 24, 32, 32, 32, 64, 64, 64, 64,  96,  96,  96, 160, 160, 160, 320, 1280]  # 1.0x
+      Out = [48, 24, 32, 32, 48, 48, 48, 88, 88, 88, 88, 136, 136, 136, 224, 224, 224, 448, 1792]  # 1.4x
+      Expansion = [1, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
+
+      with tf.variable_scope('init'):
+        x = self.conv(x, 3, Out[0], stride=2)
+        x = self.batch_norm(x)
+        x = self.activation(x)
+
+      for i in range(len(Stride)):
+        x = self._expanded_conv(x, 3, Out[i+1], Stride[i], Expansion[i], name='block%d' % i)
+
+      with tf.variable_scope('last'):
+        x = self.conv(x, 1, Out[-1])
+        x = self.batch_norm(x)
+        x = self.activation(x)
+      with tf.variable_scope('global_avg_pool'):
+        x = self.pool(x, 'GLO')
+
+      with tf.variable_scope('logit'):
+        x = self.dropout(x, 0.2)
+        x = self.fc(x, self.shape_y[1], name='fc', bias=True)
+
+      return x
 
 
 
