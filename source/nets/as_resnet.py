5332e7649938dca64ba89aa7069de1e510ab44c7
boluoweifenda <wushuang0717@gmail.com>
2019-07-20 18:05:52 +0800

    minor

--- source/nets/as_resnet.py
+++ source/nets/as_resnet.py
@@ -9,43 +9,10 @@ class ResNet(Net):
   def activation(self, x):
     return tf.nn.relu6(x)
 
-  def _group_shift(self, x, active=False):
+  def group_shift(self, x, active=False):
 
     c_in = self.get_shape(x)[1]
-
-    if self.shift_init in ['heur9', 'heur8', 'heur124']:
-      kernel = [
-        [-1, -1],
-        [ 0, -1],
-        [ 1, -1],
-        [-1,  0],
-        [ 0,  0],
-        [ 1,  0],
-        [-1,  1],
-        [ 0,  1],
-        [ 1,  1],
-      ]
-
-      if self.shift_init is 'heur8':
-        kernel.pop(4)
-      if self.shift_init is 'heur124':
-        ratio = [1,2,1,2,4,2,1,2,1]
-      else:
-        ratio = [1]*len(kernel)
-
-      num_ratio = sum(ratio)
-      assert c_in % num_ratio == 0, 'channel can not be divided by %d' % num_ratio
-
-      kernels = []
-      for i in range(len(kernel)):
-        repeat = int(c_in/num_ratio*ratio[i])
-        kernels += repeat*[kernel[i]]
-
-      kernels = np.transpose(np.array(kernels, dtype=np.float32))
-
-    elif self.shift_init is 'uniform':
-      kernels = np.random.uniform(low=-1, high=1, size=[2, c_in])
-
+    kernels = np.random.uniform(low=-1, high=1, size=[2, c_in])
     if active:
       kernels = tf.get_variable(name='shift', shape=[2, c_in], initializer=tf.initializers.constant(kernels), dtype=tf.float32)
     else:
@@ -55,7 +22,7 @@ class ResNet(Net):
     return x
 
   # pre-activation residual block
-  def _residual(self, x, c_out, stride=1, active=True, mode='shift'):
+  def residual(self, x, c_out, stride=1, active=True):
     c_in = self.get_shape(x)[1]
     shortcut = x
 
@@ -79,7 +46,7 @@ class ResNet(Net):
       # x = self.conv(x, 3, c_out / 4, stride)  # stride 2 in 3x3 conv following fb.resnet.torch
       if stride is not 1:
         x = self.pool(x, 'AVG', ksize=2, stride=2)
-      x = self._group_shift(x, active=active)
+      x = self.group_shift(x, active=active)
     with tf.variable_scope('C2'):
       x = self.conv(x, 1, c_out)
 
@@ -88,9 +55,6 @@ class ResNet(Net):
   def model(self, x):
 
     active = True
-    self.shift_init = 'uniform'
-    mode = 'shift'
-
     num_residual = [1, 3, 4, 6, 3]  # 50
     strides = [1, 2, 2, 2, 2]
     filters = [68, 68, 136, 272, 544]  # 0.5x
@@ -101,7 +65,7 @@ class ResNet(Net):
     for i in range(len(num_residual)):
       for j in range(num_residual[i]):
         with tf.variable_scope('U%d-%d' % (i, j)):
-          x = self._residual(x, filters[i], stride=strides[i] if j is 0 else 1, active=active)
+          x = self.residual(x, filters[i], stride=strides[i] if j is 0 else 1, active=active)
 
     with tf.variable_scope('global_avg_pool'):
       x = self.batch_norm(x)
